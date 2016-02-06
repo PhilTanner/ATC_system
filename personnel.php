86f93b903782bf8498b19e7aab901826a9edc3ed
Phil Tanner <phil.github@philtanner.com>
2016-02-06 15:01:33 +1300

    Update DB records

--- personnel.php
+++ personnel.php
@@ -4,16 +4,31 @@
 	
 	$id = ( isset($_GET['id'])?(int)$_GET['id']:null );
 	$user = $ATC->get_personnel($id);
+
+	if( isset( $_POST['personnel_id'] ) && isset( $_GET['id'] ) )
+	{
+		$accessrights = 0;
+		foreach( $_POST['access_rights'] as $permission )
+			$accessrights+=(int)$permission;
+		$_POST['access_rights'] = $accessrights;
+		
+		foreach( $_POST as $var => $val )
+			$user->$var = $val;
+		
+		$ATC->set_personnel( $user );
+		var_dump($user);
+	}
+	
 	
 	$ATC->gui_output_page_header('Personnel');
 	
 	if( is_object($user) )
 	{	
 ?>
-		<form id="personnelform" method="post">
+		<form id="personnelform" method="post" action="?id=<?=$user->personnel_id?>">
 			<h2> Personal details </h2>
 			<fieldset id="personal">
-				<input type="hidden" name="personnel_id" value="0" />
+				<input type="hidden" name="personnel_id" value="" />
 				<label for="firstname">First name</label>
 				<input type="text" name="firstname" id="firstname" value="" maxlength="50" required="required" placeholder="First name" /> <br />
 				<label for="lastname">Last name</label>
@@ -29,29 +44,29 @@
 
 			<h2> Access Rights </h2>
 			<fieldset id="accessrights">
-				<input type="checkbox" name="access_rights" id="access_rights_admin" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_admin" value="<?=ATC_USER_LEVEL_ADMIN?>" />
 				<label for="access_rights_admin">Admin</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_cadet" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_cadet" value="<?=ATC_USER_LEVEL_CADET?>" />
 				<label for="access_rights_cadet">Cadet</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_jnco" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_jnco" value="<?=ATC_USER_LEVEL_JNCO?>" />
 				<label for="access_rights_jnco">Junior NCO</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_snco" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_snco" value="<?=ATC_USER_LEVEL_SNCO?>" />
 				<label for="access_rights_snco">Senior NCO</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_officer" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_officer" value="<?=ATC_USER_LEVEL_OFFICER?>" />
 				<label for="access_rights_officer">Officer</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_adjutant" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_adjutant" value="<?=ATC_USER_LEVEL_ADJUTANT?>" />
 				<label for="access_rights_adjutant">Adjutant</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_stores" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_stores" value="<?=ATC_USER_LEVEL_STORES?>" />
 				<label for="access_rights_stores">Stores</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_training" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_training" value="<?=ATC_USER_LEVEL_TRAINING?>" />
 				<label for="access_rights_training">Training</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_cucdr" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_cucdr" value="<?=ATC_USER_LEVEL_CUCDR?>" />
 				<label for="access_rights_cucdr">Cadet Unit Commander</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_supoff" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_supoff" value="<?=ATC_USER_LEVEL_SUPOFF?>" />
 				<label for="access_rights_supoff">Supplimentary Officer</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_treasurer" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_treasurer" value="<?=ATC_USER_LEVEL_TREASURER?>" />
 				<label for="access_rights_treasurer">Treasurer</label><br />
-				<input type="checkbox" name="access_rights" id="access_rights_usc" value="" />
+				<input type="checkbox" name="access_rights[]" id="access_rights_usc" value="<?=ATC_USER_LEVEL_USC?>" />
 				<label for="access_rights_usc">Unit Support Committee</label><br />
 				<button type="submit">Save</button>
 			</fieldset>
@@ -63,6 +78,7 @@
 			$(function(){
 					user = jQuery.parseJSON( '<?= json_encode( $user ) ?>' );
 
+					$('#personnel_id').val(user['personnel_id']);
 					$('#firstname').val(user['firstname']);
 					$('#lastname').val(user['lastname']);
 					$('#email').val(user['email']);
