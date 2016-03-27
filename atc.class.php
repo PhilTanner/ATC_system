9ad1395106e20b7091034d58a91afd5ed062fa8a
Phil Tanner <phil@philtanner.com>
2016-03-28 11:54:38 +1300

    Release candidate post testing.

--- atc.class.php
+++ atc.class.php
@@ -4,7 +4,7 @@
 	else
 		define( 'ATC_DEBUG', 					1 );
 	
-	define( 'ATC_VERSION',						'0.7.1' );
+	define( 'ATC_VERSION',						'0.7.2' );
 	
 	// Permissions structure, as a bitmask
 	define( 'ATC_PERMISSION_PERSONNEL_VIEW', 		1 );
